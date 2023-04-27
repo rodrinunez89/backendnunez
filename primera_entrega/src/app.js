62ccb2fa75e7ecdb3da44c5b2185600f7a773ae4
Rodrigo Nuñez <rodrinunez89@icloud.com>
2023-04-27 17:47:15 -0300

    web&handle

--- primera_entrega/src/app.js
+++ primera_entrega/src/app.js
@@ -2,10 +2,20 @@ import express from "express";
 import productsRouter from './routes/products.routes.js';
 import cartRouter from './routes/cart.routes.js';
 import { __dirname } from "./utils.js";
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io'; // Se creará a partir del server HTTP
 
-const puerto = 3000;
+
+
+const puerto = parseInt(process.env.puerto || 3000);
+const ws_puerto = parseInt(process.env.ws_puerto || 3050);
 
 const server = express();
+const httpServer = server.listen(ws_puerto, () => {
+    console.log(`Servidor socketio iniciado en puerto ${WS_PORT}`);
+});
+
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
@@ -13,8 +23,14 @@ server.use('/api/products',productsRouter);
 server.use('/api/carts',cartRouter);
 
 
+// Motor de plantillas
+server.engine('handlebars', engine());
+server.set('views', './views');
+server.set('view engine', 'handlebars');
 
 
+// Contenidos estáticos
+server.use('/public', express.static(`${__dirname}/public`));
 
 server.listen (puerto, () => {
 
